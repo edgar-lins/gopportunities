3dd8e01df2b714de9392ca772cf27e1042a85108
Edgar Lins <linss.edgar@gmail.com>
2025-09-04 17:45:11 -0300

    feat: add custom logger

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/edgar-lins/gopportunities.git/router"
+import (
+	"github.com/edgar-lins/gopportunities.git/config"
+	"github.com/edgar-lins/gopportunities.git/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Failed to initialize configs: %v", err)
+		return
+	}
 	// Initialize the router and start the server
 	router.Initialize()
 }
